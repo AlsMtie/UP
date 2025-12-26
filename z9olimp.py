820a746b58e25a73eafa7b857d9a40c2588d33e3
AlsMtie <133372067+AlsMtie@users.noreply.github.com>
2025-12-27 00:46:07 +0900

    Олимпиадное программирование
    доработано

--- z9olimp.py
+++ z9olimp.py
@@ -1,23 +1,64 @@
-otvet = []
-
+#1
+"""def firstmin(n):
+    if len(n) > 1 and n[0] > n[1]:
+        return firstmin(n[1:])
+    return n
 def f(n):
-    if n[0] > n[1]:
-        return f(n[1:])
-    pervoe = n[0]
-    otvet.append(pervoe)
-    n = n[1:]
-    min_n = n[0]
-    ii = 0
-    for i in range(n[0], len(n)-1):
-        if n[i] < min_n and n[i] > pervoe:
-            min_n = n[i]
-            ii = i
-    return f(n[ii:])
+    if not n:
+        return
+    if not otvet:
+        otvet.append(n[0])
+    min_n = None
+    idx_min = None
+    for i in range(len(n)):
+        if n[i] > otvet[-1]:
+            if min_n is None or n[i] < min_n:
+                min_n = n[i]
+                idx_min = i
+    if idx_min:
+        otvet.append(min_n)
+        f(n[idx_min:])
+n = [6, 2, 5, 4, 2, 5, 6]
+otvet = []
+print(n)
+n = firstmin(n)
+f(n)
+print(otvet)"""
 
-w = [6, 2, 5, 4, 7]
 
-print(otvet)
-
+#2
+"""def firstmin(n):
+    if len(n) > 1 and n[0] > n[1]:
+        return firstmin(n[1:])
+    return n
+def posl(n):
+    if not n:
+        return
+    if not otvet:
+        otvet.append(n[0])
+    min_n = None
+    idx_min = None
+    for i in range(len(n)):
+        if n[i] > otvet[-1]:
+            if min_n is None or n[i] < min_n:
+                min_n = n[i]
+                idx_min = i
+    if idx_min:
+        otvet.append(min_n)
+        posl(n[idx_min:])
+with open('r.txt', 'r') as f:
+      l = f.readlines()
+l = str(l[1])
+l = l.split()
+spisok = []
+for el in l:
+    spisok.append(int(el))
+n = firstmin(spisok)
+otvet = []
+posl(n)
+with open("o.txt", "w") as f:
+    f.write(f"{len(otvet)}")
+print(len(otvet))"""
 #3
 """count = 0
 for i in range(100, 1050):
